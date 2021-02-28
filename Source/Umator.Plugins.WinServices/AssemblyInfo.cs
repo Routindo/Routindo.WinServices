c4a6b3cfccea6b1b80d6c28ee891074b0ed61c3f
Younes Cheikh <younes.cheikh@gmail.com>
2021-02-28 22:37:58 +0100

    Prevent contract librarires from being copied to outputp + renamed plugin

--- Source/Umator.Plugins.WinServices/AssemblyInfo.cs
+++ Source/Umator.Plugins.WinServices/AssemblyInfo.cs
@@ -2,6 +2,6 @@
 
 [assembly: UmatorPlugin(
     "186697E9-EECA-414C-8C55-CB0AC6B1CC9F",
-    "Umator Windows Services",
+    "Umator.WinServices",
     "Plugin for Windows Services Status management, it notifies when a Windows Service status is changed, or can schedule a status changing for a specific service", Author = "Younes CHEIKH", AuthorUri = "https://somecode.net", DocumentationUri = "https://somecode.net/umator-win-svc", SupportUri = "https://somecode.net/umator-win-svc"
 )]
