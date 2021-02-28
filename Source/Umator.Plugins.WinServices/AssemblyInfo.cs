ad4c87979ad2ec05caf0162205493cadf75b7489
Younes Cheikh <younes.cheikh@gmail.com>
2021-02-28 22:40:22 +0100

    Updated Author and Links

--- Source/Umator.Plugins.WinServices/AssemblyInfo.cs
+++ Source/Umator.Plugins.WinServices/AssemblyInfo.cs
@@ -3,5 +3,9 @@
 [assembly: UmatorPlugin(
     "186697E9-EECA-414C-8C55-CB0AC6B1CC9F",
     "Umator.WinServices",
-    "Plugin for Windows Services Status management, it notifies when a Windows Service status is changed, or can schedule a status changing for a specific service", Author = "Younes CHEIKH", AuthorUri = "https://somecode.net", DocumentationUri = "https://somecode.net/umator-win-svc", SupportUri = "https://somecode.net/umator-win-svc"
+    "Plugin for Windows Services Status management, it notifies when a Windows Service status is changed, or can schedule a status changing for a specific service", 
+    Author = "Umator",
+    AuthorUri = "https://umator.com",
+    DocumentationUri = "https://umator.com/docs/umator-winservices/",
+    SupportUri = "https://umator.com/support/forum/?view=forum&id=12"
 )]
